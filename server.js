15b31aff05cf6dcf9d1a440e89005c9e4b628323
Ritik Singh <singh763@purdue.edu>
2023-12-10 05:22:47 -0500

    gcloud attempt

--- server.js
+++ server.js
@@ -7,25 +7,64 @@ var cors = require('cors')
 const app = express();
 app.use(cors());
 
-const connection = mysql.createConnection({
-    host: '104.197.211.214',
-    user: 'root',
-    password: 'trashPass123',
-    database: 'tennisschema'
+// const connection = mysql.createConnection({
+//     host: '104.197.211.214',
+//     user: 'root',
+//     password: 'trashPass123',
+//     database: 'tennisschema'
+// });
+
+const mysql = require('mysql2/promise');
+const {Connector} = require('@google-cloud/cloud-sql-connector');
+
+const connector = new Connector();
+const clientOpts = await connector.getOptions({
+  instanceConnectionName: 'cs348project-407705:us-central1:myinstance',
+  ipType: 'PUBLIC',
 });
+const pool = await mysql.createPool({
+  ...clientOpts,
+  user: 'root',
+  password: 'trashPass123',
+  database: 'tennisschema',
+});
+const conn = await pool.getConnection();
+
+const [result] = await conn.query(`SELECT NOW();`);
+console.table(result); // prints returned time value from server
+
+// await pool.end();
+// connector.close();
+
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'password',
+//     database: 'tennisschema'
+// });
 
 connection.connect();
 
-app.get('/players', (req, res) => {
-    const query = "SELECT * FROM players";
-    connection.query(query, (error, results) => {
-        console.log(results);
-        if (error) {
-            res.status(500).send("Error loading data.");
-        } else {
-            res.json(results);
-        }
-    });
+app.get('/', (req, res) => {
+    console.log("opening index.html");
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/players', async (req, res) => {
+    console.log("in player route");
+    const query = "SELECT * FROM tennisschema.players";
+    const [result] = await conn.query(`SELECT * FROM tennisschema.players;`);
+    console.log("from player route: "+result);
+    res.json(result);
+    // connection.query(query, (error, results) => {
+    //     console.log("from player route: "+results);
+    //     if (error) {
+    //         res.status(500).send("Error fetching player data in server.");
+    //     } else {
+    //         res.json(results);
+    //     }
+    // });
 });
 
 app.get('/tournaments', (req, res) => {
@@ -127,6 +166,7 @@ app.post('/prizemoneyByPlayer', bodyParser.json(), (req, res) => {
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
+// const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port tester ${PORT}`);
 });
